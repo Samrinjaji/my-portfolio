7f3edfb47dc2f9ff88e16a9958ede6a3c4a9e035
Samrinjaji <samrinjaji16@gmail.com>
2026-02-05 13:41:37 +0800

    homepage done and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// button
 const menuBtn = document.getElementById('menuBtn');
 const nav = document.getElementById('navigation-menu');
 const overlay = document.getElementById('overlay');
@@ -11,10 +12,34 @@ menuBtn.addEventListener('click', () => {
     menuBtn.setAttribute('aria-expanded', isExpanded);
 });
 
-// Close menu when clicking overlay
+//  overlay
 overlay.addEventListener('click', () => {
     nav.classList.remove('active');
     menuBtn.classList.remove('active');
     overlay.classList.remove('active');
     menuBtn.setAttribute('aria-expanded', 'false');
+});
+
+// role typing
+document.addEventListener("DOMContentLoaded", () => {
+  const el = document.querySelector(".typing");
+  const text = el.dataset.text;
+  let index = 0;
+
+  const speed = 100;
+  const startDelay = 800;
+
+  setTimeout(() => {
+    const type = () => {
+      if (index < text.length) {
+        el.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, speed);
+      } else {
+        
+        el.classList.add("done");
+      }
+    };
+    type();
+  }, startDelay);
 });
